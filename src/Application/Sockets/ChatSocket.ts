0c550b560ba14a75327b8c6084d471ec864bbdcb
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-09-05 19:55:52 -0300

    Re-estructuracion de la implementacion del Socket

---
+++ src/Application/Sockets/ChatSocket.ts
@@ -0,0 +1,52 @@
+import { Server } from "socket.io";
+import passport from '../../Infrastructure/Config/Passport'
+import IChatCommand from "../../Infrastructure/Interfaces/IChatCommand";
+import ChatCommand from "../../Infrastructure/Command/ChatCommand";
+import ChatQuery from "../../Infrastructure/Query/ChatQuery";
+import IChatQuery from "../../Infrastructure/Interfaces/IChatQuery";
+import IChatServices from "../../Domain/Interfaces/IChatServices";
+import ChatServices from "../../Domain/Services/ChatServices";
+import IncomingMessageDTO from "../DTO/IncomingMessageDTO";
+
+
+const chatCommand: IChatCommand = new ChatCommand();
+const chatQuery: IChatQuery = new ChatQuery();
+const chatServices: IChatServices = new ChatServices(chatCommand, chatQuery);
+
+function ChatSocket(io: Server) {
+    // Deberia utilizar el tipo correcto
+    io.engine.use((req: any, res: any, next: any) => {
+        const isHandshake = req._query.sid === undefined;
+        if (isHandshake) {
+            passport.authenticate("jwt", { session: false })(req, res, next);
+        } else {
+            next();
+        }
+    });
+
+    io.on('connection', function (socket) {
+        console.log('A user connected');
+        const user = socket.request.user;
+        if (user) {
+            console.log('User id: ', user.id);
+        }
+        socket.on('join', async (chatId) => {
+            console.log('A user joined a room');
+            socket.join(chatId);
+        })
+        socket.on('msg', async (msg: IncomingMessageDTO, chatId: string) => {
+            const createdMessage = await chatServices.sendMessage(msg);
+            const sockets = await io.in(chatId).fetchSockets();
+            // Si hay mas de un socket por mismo usuario puede generar duplicados
+            if (sockets.length > 1) {
+                // Tal vez es necesario eliminar la propiedad IsRead al enviarlo
+                socket.to(chatId).emit('msg', createdMessage);
+            }
+            else {
+                // TODO
+                // Crear notificacion en el microservicio de notificaciones
+            }
+        })
+    })
+}
+export default ChatSocket;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
 import app from "./app"
 import { createServer, IncomingMessage } from 'node:http';
 import { Server } from "socket.io";
-import IChatServices from "./Domain/Interfaces/IChatServices";
-import ChatServices from "./Domain/Services/ChatServices";
-import IncomingMessageDTO from "./Application/DTO/IncomingMessageDTO";
-import IChatCommand from "./Infrastructure/Interfaces/IChatCommand";
-import ChatCommand from "./Infrastructure/Command/ChatCommand";
-import IChatQuery from "./Infrastructure/Interfaces/IChatQuery";
-import ChatQuery from "./Infrastructure/Query/ChatQuery";
-import passport from "./Infrastructure/Config/Passport";
-
-
+import ChatSocket from "./Application/Sockets/ChatSocket";
 
 interface User
 {
@@ -32,47 +23,7 @@ const io = new Server(server,
         },
         maxHttpBufferSize: 4e6 // 4Mb
     });
-
-const chatCommand: IChatCommand = new ChatCommand();
-const chatQuery: IChatQuery = new ChatQuery();
-const chatServices: IChatServices = new ChatServices(chatCommand, chatQuery);
-
-// Deberia utilizar el tipo correcto
-io.engine.use((req: any, res: any, next: any) => {
-    const isHandshake = req._query.sid === undefined;
-    if (isHandshake) {
-        passport.authenticate("jwt", { session: false })(req, res, next);
-    } else {
-        next();
-    }
-});
-
-io.on('connection', function (socket) {
-    console.log('A user connected');
-    const user = socket.request.user;
-    if(user)
-        {
-            console.log('User id: ', user.id);
-        }    
-    socket.on('join', async (chatId) => {
-        console.log('A user joined a room');
-        socket.join(chatId);
-    })
-    socket.on('msg', async (msg: IncomingMessageDTO, chatId: string) => {
-            const createdMessage = await chatServices.sendMessage(msg);         
-            const sockets = await io.in(chatId).fetchSockets();
-            // Si hay mas de un socket por mismo usuario puede generar duplicados
-            if (sockets.length > 1) {
-                // Tal vez es necesario eliminar la propiedad IsRead al enviarlo
-                socket.to(chatId).emit('msg', createdMessage);
-            }
-            else {
-                // TODO
-                // Crear notificacion en el microservicio de notificaciones
-            }
-    })    
-})
-
+ChatSocket(io);
 // Iniciamos el servidor en el puerto 3003
 server.listen(PORT, function () {
     console.log(`Servidor iniciado en http://localhost:${PORT}`)
