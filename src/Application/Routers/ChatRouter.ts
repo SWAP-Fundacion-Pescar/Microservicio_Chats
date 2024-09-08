1af0ca7591a27768182208a23bd90a78c4a8160e
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-09-07 22:53:21 -0300

    Validation Error Handler agregado a las rutas

--- src/Application/Routers/ChatRouter.ts
+++ src/Application/Routers/ChatRouter.ts
@@ -8,6 +8,7 @@ import IChatServices from "../../Domain/Interfaces/IChatServices";
 import ChatServices from "../../Domain/Services/ChatServices";
 import { validateCreateChat, validateReadMessage, validateDeleteChat } from "../Middleware/Validator/ChatValidator";
 import { authenticateJwt } from "../Middleware/PassportMiddleware";
+import validationErrorHandler from "../Middleware/Validator/ValidationErrorHandler";
 
 
 
@@ -16,9 +17,9 @@ const chatCommand: IChatCommand = new ChatCommand();
 const chatServices: IChatServices = new ChatServices(chatCommand, chatQuery);
 const chatController: ChatController = new ChatController(chatServices);
 const ChatRouter = Router();
-ChatRouter.post('/chat', validateCreateChat, chatController.createChat);
-ChatRouter.delete('/chat/:chatId', validateDeleteChat, chatController.deleteChat);
+ChatRouter.post('/chat', validateCreateChat, validationErrorHandler, chatController.createChat);
+ChatRouter.delete('/chat/:chatId', validateDeleteChat, validationErrorHandler, chatController.deleteChat);
 ChatRouter.get('/chats/:userId', chatController.getChatsByUserId);
-ChatRouter.put('/chats/read', validateReadMessage, chatController.readMessage);
+ChatRouter.put('/chats/read', validateReadMessage, validationErrorHandler, chatController.readMessage);
 
 export default ChatRouter;
